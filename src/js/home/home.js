7ced5b15beec92da63ed71c984d475eb305a5683
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-04-15 18:20:59 +0300

    fixed style of img and add dataset-id

--- src/js/home/home.js
+++ src/js/home/home.js
@@ -25,9 +25,9 @@ function renderBestSellersBooks(data){
     `<li class="gallery__item">
     <a class="gallery__link" href="#">${list_name}</a>
     <ul class="card-container">
-    ${books.map(({ book_image, title, author }) =>
+    ${books.map(({ book_image, title, author, _id }) =>
     `<li class="card-container__item">
-    <button type="button" class="card-container__link" href="#">
+    <button type="button"  data-id="${_id}" class="card-container__link">
         <div class="card-container__thumb">
         <img src="${book_image}" alt="book-image" loading="lazy" width="180px" height="256px" class="card-container__image" />
         <div class="overlay">
@@ -60,18 +60,17 @@ function onClick(e){
 
     if (e.target.nodeName === "A") {
         const searchCategotyByLink = e.target.textContent;
-           console.log(searchCategotyByLink); 
         } else 
            
     
-    if (e.target.nodeName === "IMG") {
-        const searchCard = e.target.parentNode.nextElementSibling.firstElementChild.textContent;
-           console.log(searchCard); 
-           } else 
-
     if (e.target.nodeName === "BUTTON") {
-        const searchCategotyByBtn = e.target.previousElementSibling.previousElementSibling.textContent;
-        console.log(searchCategotyByBtn);   
+
+        if (e.target.classList.contains('card-container__link')) {
+            const searchCardById = e.target.dataset.id;
+            return;
+    } 
+     const searchCategotyByBtn = e.target.previousElementSibling.previousElementSibling.textContent;
+          
       }
         
 
