52f9208fc290977637436fa74ecb3c9a362e7a66
DisRachik <radchenkoig@gmail.com>
2023-04-16 14:26:03 +0300

    user registration with a name has been implemented

--- src/js/authorization-modal/authorization-modal.js
+++ src/js/authorization-modal/authorization-modal.js
@@ -1,5 +1,11 @@
-import { btnsGroupForChangeForm } from './refsForm';
-import { onBtnReplaceForm, closeModalForm } from './form';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../firebase/firebaseConfig';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+import { btnsGroupForChangeForm, formSignUp, formSignIn } from './refsForm';
+import { onBtnReplaceForm, closeModalForm, onFormSubmit } from './form';
 
 btnsGroupForChangeForm.addEventListener('click', onBtnReplaceForm);
 closeModalForm();
+
+formSignUp.addEventListener('submit', onFormSubmit);
--- src/js/authorization-modal/form.js
+++ src/js/authorization-modal/form.js
@@ -1,4 +1,21 @@
-import { formsForRegistration, bodyEl, backdropEl, btnClose } from './refsForm';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../firebase/firebaseConfig';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+import {
+  formsForRegistration,
+  bodyEl,
+  backdropEl,
+  btnClose,
+  formSignUp,
+  formSignIn,
+} from './refsForm';
+import { createNewUser } from '../firebase/fetchFirebace';
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const formData = {};
 
 export const onBtnReplaceForm = e => {
   if (e.currentTarget === e.target) {
@@ -20,6 +37,7 @@ function changeElem(arrEl) {
   });
 }
 
+// Для закриття форми реєстрації
 const onBackdropClick = e => {
   if (e.currentTarget !== e.target) {
     return;
@@ -35,3 +53,36 @@ export function closeModalForm() {
   backdropEl.addEventListener('click', onBackdropClick);
   btnClose.addEventListener('click', onBtnClose);
 }
+
+// Сабміт форм
+export const onFormSubmit = e => {
+  e.preventDefault();
+
+  if (!formData.name || !formData.mail || !formData.password) {
+    alert('fake');
+    return;
+  }
+
+  createNewUser(formData).then(console.log);
+
+  // createUserWithEmailAndPassword(auth, formData.mail, formData.password)
+  //   .then(userCredential => {
+  //     // Signed in
+  //     const user = userCredential.user;
+  //     console.log(user);
+  //     // ...
+  //   })
+  //   .catch(error => {
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //     // ..
+  //   });
+
+  e.currentTarget.reset();
+};
+
+const onFormData = e => {
+  formData[e.target.name] = e.target.value.trim();
+};
+
+formSignUp.addEventListener('input', onFormData);
