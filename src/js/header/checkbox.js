35a8c676839bd5aaf40e7c41d6c77be06da4234d
8Yurii8 <114691896+8Yurii8@users.noreply.github.com>
2023-04-17 13:58:11 +0300

    checkBox add localStorage adn correct buttons

--- src/js/header/checkbox.js
+++ src/js/header/checkbox.js
@@ -1,7 +1,22 @@
-function changeTheme(isChecked) {
+window.changeTheme = function (isChecked) {
   if (isChecked) {
     document.body.setAttribute('dark', '');
+    localStorage.setItem('theme', 'dark');
   } else {
     document.body.removeAttribute('dark');
+    localStorage.setItem('theme', 'light');
+  }
+};
+
+function applySavedTheme() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.body.setAttribute('dark', '');
+    document.querySelector('#highload1').checked = true;
+  } else {
+    document.body.removeAttribute('dark');
+    document.querySelector('#highload1').checked = false;
   }
 }
+
+applySavedTheme();
