5a5904afaca8ca9e3bacb3fb42f11ac49f9367d9
GrabDmitro <d_grab2001@ukr.net>
2023-04-18 12:11:37 +0300

    storage-add

--- src/js/pop-up/pop-up.js
+++ src/js/pop-up/pop-up.js
@@ -1,25 +1,33 @@
 import NewsApiBooksService from '../api-service/api-service';
 
-const refs = {
-  modalRef: document.querySelector('.modal'),
-  backdropRef: document.querySelector('.backdrop'),
-  coverRef: document.querySelector('.modal_cover'),
-  book_nameRef: document.querySelector('.modal_book_name'),
-  authorRef: document.querySelector('.modal_author'),
-  discriptionRef: document.querySelector('.modal_discription'),
-  shopsRef: document.querySelector('.modal_shops'),
-  buttonRef: document.querySelector('.modal_button'),
-  info_textRef: document.querySelector('.modal_info_text'),
-  imgRef: document.querySelector('.modal_img'),
-  coverImg: document.querySelector('.modal_img_placeholder'),
-  itemsRef: document.querySelectorAll('.modal_item'),
-};
-
-const api = new NewsApiBooksService();
 function renderModal(id) {
+  const api = new NewsApiBooksService();
+  const refs = {
+    modalRef: document.querySelector('.modal'),
+    backdropRef: document.querySelector('.backdrop'),
+
+    book_nameRef: document.querySelector('.modal_book_name'),
+    authorRef: document.querySelector('.modal_author'),
+    discriptionRef: document.querySelector('.modal_discription'),
+
+    buttonRef: document.querySelector('.modal_button'),
+    info_textRef: document.querySelector('.modal_info_text'),
+    imgRef: document.querySelector('.modal_img'),
+    coverImg: document.querySelector('.modal_img_placeholder'),
+    itemsRef: document.querySelectorAll('.modal_item'),
+    closeRef: document.querySelector('.modal_close'),
+  };
+  if (!localStorage.getItem('basket')) {
+    localStorage.setItem('basket', '[]');
+  }
+  let storage = JSON.parse(localStorage.getItem('basket'));
+
+  const markup = refs.modalRef.innerHTML;
+
   api.GetBooksById(id).then(load);
   function load(data) {
-    console.log(data);
+    renderBtn();
+    refs.backdropRef.classList.remove('is-hidden');
     if (data.book_image) {
       refs.coverImg.src = data.book_image;
       refs.coverImg.style = 'height:100%; width:100%';
@@ -29,7 +37,7 @@ function renderModal(id) {
     }
 
     if (data.author) {
-      refs.autorRef.innerText = data.author;
+      refs.authorRef.innerText = data.author;
     }
 
     if (data.description) {
@@ -50,41 +58,44 @@ function renderModal(id) {
         refs.itemsRef[2].firstElementChild.href = shop.url;
       }
     });
+
+    refs.closeRef.addEventListener('click', closeModal);
+    refs.backdropRef.addEventListener('click', closeModal);
+    function removeFromStorage() {
+      const newBasket = storage.filter(el => el == JSON.stringify(id));
+
+      localStorage.setItem('basket', newBasket);
+      refs.buttonRef.removeEventListener('click', removeFromStorage);
+      storage = newBasket;
+      renderBtn();
+    }
+    function addToStorage() {
+      storage.push(data._id);
+
+      localStorage.setItem('basket', JSON.stringify(storage));
+      refs.buttonRef.removeEventListener('click', addToStorage);
+
+      renderBtn();
+    }
+    function renderBtn() {
+      if (storage.includes(data._id)) {
+        refs.buttonRef.innerText = 'remove from the shopping list';
+        refs.info_textRef.classList.remove('is-hidden');
+        refs.buttonRef.addEventListener('click', removeFromStorage);
+      } else {
+        refs.buttonRef.innerText = 'add to shopping list';
+        refs.info_textRef.classList.add('is-hidden');
+        refs.buttonRef.addEventListener('click', addToStorage);
+      }
+    }
+  }
+
+  function closeModal(e) {
+    if (e.target == refs.backdropRef || e.target == refs.closeRef) {
+      refs.modalRef.innerHTML = markup;
+      refs.backdropRef.classList.add('is-hidden');
+      refs.closeRef.removeEventListener('click', closeModal);
+      refs.backdropRef.removeEventListener('click', closeModal);
+    }
   }
 }
-renderModal('643282b1e85766588626a080');
-
-function closeModal() {
-  refs.modalRef.innerHTML = `<div class="modal_top_section">
-            <div class="modal_cover">
-                <img src="/Logotype.b4addec0.svg" alt="bookcover" class="modal_img_placeholder">
-            </div>
-            <div class="modal_info_section">
-                <p class="modal_book_name">N/A</p>
-                <p class="modal_author">N/A</p>
-                <p class="modal_discription">N/A</p>
-                <ul class="modal_shops">
-                    <li class="modal_item is-hidden" style="width: 62px">
-                        <a href="">
-                            <img src="/amazon.db4f508c.png" alt="amazon" class="modal_shop_img" style="height: 19px; width: 62px">
-                        </a>
-                    </li>
-                    <li class="modal_item is-hidden" style="width: 32px">
-                        <a href="">
-                            <img src="/apple-books.0196ca03.png" alt="amazon" class="modal_shop_img" style="height: 32px; width: 33px"></a>
-                    </li>
-                    <li class="modal_item is-hidden" style="width: 38px">
-                        <a href="">
-                            <img src="/bookshop.a838b065.png" alt="amazon" class="modal_shop_img" style="height: 38px; width: 36px"></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="modal_bottom_container">
-            <button class="modal_button">remove from the shopping list</button>
-            <p class="modal_info_text ">Сongratulations! You have added the book to the shopping list. To
-                delete, press
-                the button “Remove from the shopping list”.</p>
-        </div>`;
-}
-closeModal();
