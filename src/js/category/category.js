f70dd391097eb9c9ac72d91b19a6c8700ce44005
Alona37 <113711181+Alona37@users.noreply.github.com>
2023-04-20 10:41:38 +0300

    fixed

--- src/js/category/category.js
+++ src/js/category/category.js
@@ -3,11 +3,36 @@ import { Notify } from 'notiflix';
 import renderCardListByCategory from './renderCatdListByCategory';
 import updateTitle from './updateTitle';
 
-const containerEl = document.querySelector('.categories__container');
-const button = document.querySelector('.categories__btn');
-
 const newsApiBooksService = new NewsApiBooksService();
 
+const btnAllCategory = document.querySelector('.btn');
+
+// вызов всех категорий (Топ-категории)
+
+btnAllCategory.addEventListener('click', onClickBtn);
+function onClickBtn(event) {
+  if (event.target.classList !== 'categories__btn--active') {
+    btnAllCategory.classList.remove('categories__btn--active');
+  } else {
+    btnAllCategory.classList.add('categories__btn--active');
+  }
+  if (event.target) {
+  }
+
+  const result = `<h1 class="gallery__title">Best Sellers <span class="gallery__title--span">Books</span></h1>`;
+  const title = document.querySelector('.gallery__title');
+  title.innerHTML = result;
+  newsApiBooksService
+    .getTopBooks()
+    .then(renderCardListByCategory)
+
+    .catch(erorrQuery);
+}
+
+// Вызов отдельных категорий
+
+const containerEl = document.querySelector('.categories__container');
+const button = document.querySelector('.categories__btn');
 containerEl.addEventListener('click', onClickCategory);
 
 newsApiBooksService
--- src/js/category/updateTitle.js
+++ src/js/category/updateTitle.js
@@ -1,13 +1,12 @@
 const title = document.querySelector('.gallery__title');
 
 export default function updateTitle(nameCategory) {
-
-    const words = nameCategory.split(' ').filter(Boolean);
-    const lastWord = words[words.length - 1];
-    let str = words.slice(0, words.length - 1).join(' ');
-    const result = `<h1 class="gallery__title">
+  const words = nameCategory.split(' ').filter(Boolean);
+  const lastWord = words[words.length - 1];
+  let str = words.slice(0, words.length - 1).join(' ');
+  const result = `<h1 class="gallery__title">
       ${str} <span class="gallery__title--span">${lastWord}</span>
     </h1>`;
-    const title = document.querySelector('.gallery__title');
-    title.innerHTML = result;
-  }
+  const title = document.querySelector('.gallery__title');
+  title.innerHTML = result;
+}
